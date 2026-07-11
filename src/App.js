f9bb632095016e590471439b4ce3757229a69879
Shittu Emmanuel <shem4soul@gmail.com>
2026-07-11 22:33:57 +0100

    Add emoji to stats message for improved visual appeal

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function PackingList() {
 function Stats() {
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em> 💼You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
 }
