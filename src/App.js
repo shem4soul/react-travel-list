694f562b8018da95f3dcae3ab5e19038033dbba2
Shittu Emmanuel <shem4soul@gmail.com>
2026-07-12 10:53:03 +0100

    Add form submission handler and improve item selection in Form component

--- src/App.js
+++ src/App.js
@@ -20,9 +20,22 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <div className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your😎 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
     </div>
   );
 }
