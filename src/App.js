7d179e1e1c1922374e337cfe8a10a34aba0d283a
Shittu Emmanuel <shem4soul@gmail.com>
2026-07-12 09:07:24 +0100

    Refactor PackingList component to display initial items and add Item component for individual packing items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 5, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -24,11 +30,25 @@ function Form() {
 function PackingList() {
   return (
     <div className="list">
-      <h3>Packing List</h3>
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
     </div>
   );
 }
 
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
 function Stats() {
   return (
     <footer className="stats">
