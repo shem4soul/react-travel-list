03697de29f22c9cd4a0565d11a35731d7b472d27
Shittu Emmanuel <shem4soul@gmail.com>
2026-07-26 18:31:48 +0100

    Add Logo component and integrate it into App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Logo from "./Logo";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -39,10 +40,6 @@ export default function App() {
       <Stats items={items} />
     </div>
   );
-}
-
-function Logo() {
-  return <h1>🌴Far Away 💼</h1>;
 }
 
 function Form({ onAddItems }) {
